3c5d641fb8a89b7020ba8e842630942770bb1814
김광준 <kwangjun3952@gmail.com>
2019-05-24 11:58:37 +0900

    데이터 모델 생성

--- FishingPoint/FishingPoint/Data/FeedData.swift
+++ FishingPoint/FishingPoint/Data/FeedData.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import UIKit
+
+struct FeedData {
+    let feedImage: String!
+    let pointName: String!
+    let rodName: String!
+    let reelName: String!
+    let lureName: String!
+    
+    init(feedImage: String, pointName: String, rodName: String, reelName: String, lureName: String) {
+        self.feedImage = feedImage
+        self.pointName = pointName
+        self.rodName = rodName
+        self.reelName = reelName
+        self.lureName = lureName
+    }
+}
