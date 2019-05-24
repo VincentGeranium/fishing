8cbec05a04c7946bed6d8a9d56ba75b32fe5ede3
김광준 <kwangjun3952@gmail.com>
2019-05-24 13:03:19 +0900

    add new edit about FishingPointViewController

--- FishingPoint/FishingPoint/VC/FishingPointViewController.swift
+++ FishingPoint/FishingPoint/VC/FishingPointViewController.swift
@@ -7,24 +7,69 @@
 //
 
 import UIKit
+import MapKit
 
 class FishingPointViewController: UIViewController {
-
+    
+    var originAddress: String = ""
+    
+    private let myMap: MKMapView = {
+        let myMap = MKMapView()
+        myMap.translatesAutoresizingMaskIntoConstraints = false
+        return myMap
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        addSubView()
+        autoLayout()
+        searchMap(originAddress)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func goToAnnotation(_ originAddress: String, _ geoAddress: CLPlacemark) {
+        // span, region 설정
+        let span = MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
+        let region = MKCoordinateRegion(center: geoAddress.location!.coordinate, span: span)
+        
+        // 위도, 경도 값 저장
+        let latitude = geoAddress.location!.coordinate.latitude
+        let longitude = geoAddress.location!.coordinate.longitude
+        
+        // addAnnotaition의 위치 정보 입력
+        let destination = MKPointAnnotation()
+        destination.title = "My Point"
+        destination.subtitle = originAddress
+        destination.coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        myMap.addAnnotation(destination)
+        myMap.setRegion(region, animated: true)
     }
-    */
-
+    
+    // 맵 검색 (geocoder 사용하여 주소를 좌표로 변환)
+    private func searchMap(_ addr: String) {
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(addr) { (placeMark, error) in
+            if error != nil {
+                return print(error!.localizedDescription)
+            }
+            guard let address = placeMark?.first else { return }
+            self.goToAnnotation(addr, address)
+        }
+    }
+    
+    private func addSubView() {
+        view.addSubview(myMap)
+    }
+    
+    private func autoLayout() {
+        let guide = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            myMap.topAnchor.constraint(equalTo: guide.topAnchor),
+            myMap.leadingAnchor.constraint(equalTo: guide.leadingAnchor),
+            myMap.trailingAnchor.constraint(equalTo: guide.trailingAnchor),
+            myMap.bottomAnchor.constraint(equalTo: guide.bottomAnchor),
+            ])
+    }
 }
